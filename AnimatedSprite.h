2c7b9a5b91c92e6ea057cda839c37f8ed3b22c50
Eric <erace1994@hotmail.fr>
2015-11-02 13:23:09 -0500

    Commit "Add" AnimatedSprite
    Signed-off-by: Eric <erace1994@hotmail.fr>

---
+++ AnimatedSprite.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "Sprite.h"
+
+class AnimatedSprite :
+	public Sprite
+{
+public:
+	AnimatedSprite();
+	AnimatedSprite(std::string path, int srcTop, int srcLeft, float frameWidth, int frameHeight, int nbRow, int nbCol, int frameRate, bool looping);
+	~AnimatedSprite();
+
+	virtual void Update(float dt);
+	virtual void Draw();
+
+private:
+	
+	RECT initialRect;
+	bool isLooping;
+	int nbFrame;
+	int nbRow;
+	int nbCol;
+	int frameRate;
+
+	RECT currentRect;
+	float currentTime;
+	int currentRow;
+	int currentCol;
+
+	D3DXVECTOR3 position1;
+	float x, y, z;
+};
+
