d764a2c53073eba098b2466e1530d01cb8b7de63
alexlaf1933 <alexlaf683@gmail>
2015-11-04 09:43:51 -0500

    Update pour l'anime et le visible

--- AnimatedSprite.h
+++ AnimatedSprite.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "Sprite.h"
-
 class AnimatedSprite :
 	public Sprite
 {
@@ -9,18 +8,19 @@ public:
 	AnimatedSprite();
 	AnimatedSprite(std::string path, int srcTop, int srcLeft, float frameWidth, int frameHeight, int nbRow, int nbCol, int frameRate, bool looping);
 	~AnimatedSprite();
-
+	void SetAnimatedVisible(bool visible){ isVisible = visible; }
 	virtual void Update(float dt);
 	virtual void Draw();
 
 private:
-
+	bool isVisible;
 	RECT initialRect;
 	bool isLooping;
 	int nbFrame;
 	int nbRow;
 	int nbCol;
 	int frameRate;
+
 
 	RECT currentRect;
 	float currentTime;
