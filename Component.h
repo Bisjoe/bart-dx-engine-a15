4384daf7bfe48db13c7e7295eb097a490fca7fed
Joe Bissonnette <z3nder@gmail.com>
2015-11-19 13:37:08 -0500

    move const

--- Component.h
+++ Component.h
@@ -10,8 +10,10 @@ public:
 	Component();
 	Component(float x, float y, float z);
 	virtual ~Component();
+
 	virtual void Update(float dt) = 0;
 	virtual void Draw() = 0;
+
 	D3DXVECTOR3 GetPosition() const { return position; }
 	virtual void SetPosition(D3DXVECTOR3& pos){
 		position = pos;
@@ -20,13 +22,13 @@ public:
 		position.x = x;
 		position.y = y;
 	}
+	std::string GetID() const { return id; }
 	void SetID(std::string id){ this->id = id; }
-	const std::string GetID(){ return id; }
-	
 
 private:
 	static std::vector<Component*>components;
-	 std::string id;
-	 D3DXVECTOR3 position;
+	std::string id;
+	D3DXVECTOR3 position;
+
 };
 
